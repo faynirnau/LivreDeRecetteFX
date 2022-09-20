7f30d7de55afbff6f2ba63fbf72304c7bf10d83e
faynirnau <naudet.jeremy@laposte.net>
2022-09-20 20:21:48 +0200

    réussite d'affichage des noms des recettes dans la listeview

--- src/main/java/com/example/livrederecettefx/AppController.java
+++ src/main/java/com/example/livrederecettefx/AppController.java
@@ -1,16 +1,13 @@
 package com.example.livrederecettefx;
 
-import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 public class AppController implements Initializable {
--- src/main/java/com/example/livrederecettefx/AppLivreDeRecette.java
+++ src/main/java/com/example/livrederecettefx/AppLivreDeRecette.java
@@ -4,10 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Scanner;
 import java.io.IOException;
 
 public class AppLivreDeRecette extends Application {
